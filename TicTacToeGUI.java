bfe6b7773fc5b8c2769c3dce4e33090b4499d5f7
bhargav <bhargavvicharapu1609@gmail.com>
2025-04-09 00:17:44 -0500

    Commit 3: TicTacToe Default Constructor has been created

--- TicTacToeGUI.java
+++ TicTacToeGUI.java
@@ -3,11 +3,38 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class TicTacToeGUI {
+public class TicTacToeGUI implements ActionListener {
+
+    JFrame frame;
+    JButton[] buttons = new JButton[9];
+    boolean xTurn = true;
+
+    public TicTacToeGUI() {
+        frame = new JFrame("Tic Tac Toe");
+        frame.setSize(400, 400);
+        frame.setLayout(new GridLayout(3, 3));
+
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton("");
+            buttons[i].setFont(new Font("Arial", Font.BOLD, 60));
+            buttons[i].addActionListener(this);
+            frame.add(buttons[i]);
+        }
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
    
 
     public static void main(String[] args) {
         new TicTacToeGUI();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 
    
