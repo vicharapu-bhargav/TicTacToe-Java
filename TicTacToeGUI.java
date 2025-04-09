57c0cd0742647b97d0c88a7b142a9051278157af
bhargav <bhargavvicharapu1609@gmail.com>
2025-04-08 23:55:36 -0500

    Initial commit: Tic Tac Toe GUI file has been createdin Java

---
+++ TicTacToeGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToeGUI {
+   
+
+    public static void main(String[] args) {
+        new TicTacToeGUI();
+    }
+
+   
+}
